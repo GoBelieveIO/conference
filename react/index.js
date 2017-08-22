ada7313e40bb410c62ff09bf2d8a98adc93c9bc1
houxh <houxuehua49@gmail.com>
2017-08-22 17:48:11 +0800

    remove helloworld

--- react/index.js
+++ react/index.js
@@ -1,18 +1,5 @@
 import { AppRegistry } from 'react-native';
-//import { App } from './features/app';
-//import { ConferenceCreator } from './features/conference';
 import GroupCall from './GroupCall.js';
-import HelloWorld from './HelloWorld.js';
-
-// Register the main Component.
-//AppRegistry.registerComponent('App', () => App);
-
-// Register the main Component.
-//AppRegistry.registerComponent('ConferenceCreator', () => ConferenceCreator);
-
-
-// Register the main Component.
 AppRegistry.registerComponent('GroupCall', () => GroupCall);
 
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
 
